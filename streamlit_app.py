f75f309349b3f7d0dc4cf4bc4109012006da76fd
DannyVH24 <dannyestuardo321@gmail.com>
2025-04-06 16:01:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -126,7 +126,7 @@ elif st.session_state["main_menu"] == "Estadística 2":
     sub_options = {
         "📏 Intervalos": "Intervalos de Confianza",
         "🔍 Tamaños Muestra": "Tamaños de Muestra",
-        "📊 Generar Tabla": "Generar Tabla",
+        "📊 Generar Datos": "Generar Datos",
         "📈 Est. con Datos": "Estimación con Datos"
     }
     
